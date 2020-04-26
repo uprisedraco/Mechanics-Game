e5b409b7c086163d7c3806604bbfd91e7c6a8e4c
uprisedraco <uprisedraco@gmail.com>
2020-04-26 16:44:34 +0300

    Achievement System

---
+++ Mechanics/Assets/Scripts/AchievementSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchievementSystem : MonoBehaviour
+{
+    void Start()
+    {
+        PlayerPrefs.DeleteAll();
+
+        PointOfInterest.OnPointOfInterestEntered += POI_OnPointOfInterestEntered;
+    }
+
+    private void POI_OnPointOfInterestEntered(PointOfInterest poi)
+    {
+        string achievementKey = "Achievement " + poi.name;
+
+        if (PlayerPrefs.GetInt(achievementKey) == 1)
+            return;
+
+        PlayerPrefs.SetInt(achievementKey, 1);
+        Debug.Log("Unlocked achievement: " + poi.name);
+    }
+}
---
+++ Mechanics/Assets/Scripts/PointOfInterest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PointOfInterest : MonoBehaviour
+{
+    public static event Action<PointOfInterest> OnPointOfInterestEntered;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(OnPointOfInterestEntered != null)
+        {
+            OnPointOfInterestEntered(this);
+        }
+    }
+}
