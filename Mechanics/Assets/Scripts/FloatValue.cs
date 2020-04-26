f111a864db701395c0f4cf74c53ab831d97214fc
uprisedraco <uprisedraco@gmail.com>
2020-04-26 16:24:57 +0300

    Update

---
+++ Mechanics/Assets/Scripts/FloatValue.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class FloatValue : ScriptableObject
+{
+    public float DefaultValue;
+
+    public float Value;
+
+    public static implicit operator float(FloatValue floatValue)
+    {
+        return floatValue.Value;
+    }
+}
